4f3e886b155932bee75ccd71785b8acfbdd080fd
udodikdmitro <105604449+udodikdmitro@users.noreply.github.com>
2022-06-14 15:48:25 +0300

    Kata1 (#1)
    * Kata1 is done!

---
+++ src/katatraining/grasshopper/GrassHopper.java
@@ -0,0 +1,17 @@
+package katatraining.grasshopper;
+
+/**
+ * Debugging sayHello function
+ * The starship Enterprise has run into some problem when creating a program
+ * to greet everyone as they come aboard. It is your job to fix the code and
+ * get the program working again!
+ * <p>
+ * Example output:
+ * <p>
+ * Hello, Mr. Spock
+ */
+public class GrassHopper {
+    public static String sayHello(String name) {
+        return "Hello, " + name;
+    }
+}
---
+++ src/katatraining/grasshopper/Test.java
@@ -0,0 +1,7 @@
+package katatraining.grasshopper;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(GrassHopper.sayHello("Dima"));
+    }
+}
