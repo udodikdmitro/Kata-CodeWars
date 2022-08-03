3de02adab6753935ba22afea7f56d6d85c612457
udodikdmitro <105604449+udodikdmitro@users.noreply.github.com>
2022-08-03 23:07:46 +0300

    Kata 8, "Yes or no". (#3)
    Co-authored-by: dima udodik <dima udodik>

---
+++ src/katatraining/yesorno/YesOrNo.java
@@ -0,0 +1,12 @@
+package katatraining.yesorno;
+
+public class YesOrNo {
+
+    public static String boolToWord(boolean c) {
+        if (c) {
+            return "Yes";
+        } else {
+            return "No";
+        }
+    }
+}
