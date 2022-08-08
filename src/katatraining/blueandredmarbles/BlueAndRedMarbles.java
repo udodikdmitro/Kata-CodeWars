9ca99ad0c29486fd4613d3577fb9b77f1a725b9d
dima udodik <dima udodik>
2022-08-08 18:52:38 +0300

    Kata 8, "Blue and red marbles".

--- src/katatraining/blueandredmarbles/BlueAndRedMarbles.java
+++ src/katatraining/blueandredmarbles/BlueAndRedMarbles.java
@@ -1,9 +1,15 @@
 package katatraining.blueandredmarbles;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class BlueAndRedMarbles {
-    public static double guessBlue(int blueStart, int redStart, int bluePulled, double redPulled) {
-        double result = (blueStart - bluePulled)/(blueStart + redStart - bluePulled - redPulled);
-        return result;
+    public static double guessBlue(int blueStart, int redStart, int bluePulled, int redPulled) {
+        BigDecimal a = new BigDecimal(blueStart + redStart - bluePulled - redPulled)
+                .setScale(8, RoundingMode.FLOOR);
+        BigDecimal b = new BigDecimal(blueStart - bluePulled)
+                .setScale(8, RoundingMode.FLOOR);
+        return b.divide(a, 8, RoundingMode.FLOOR).doubleValue();
     }
 }
 
