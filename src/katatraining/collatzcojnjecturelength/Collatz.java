eeed71e87d0710d487eaab2adcf5b9dfe4722dbd
dima udodik <dima udodik>
2022-08-12 17:09:26 +0300

    Kata 7, "Collatz Conjecture Length". Fixed for zero value.

--- src/katatraining/collatzcojnjecturelength/Collatz.java
+++ src/katatraining/collatzcojnjecturelength/Collatz.java
@@ -1,8 +1,24 @@
 package katatraining.collatzcojnjecturelength;
 
+/**
+Way to solve with recursion
+public class Collatz {
+    public static long conjecture(long x) {
+        return x == 1 || x == 0 ? x : 1 + (x % 2 == 0 ?
+                conjecture(x / 2)
+                : conjecture(x * 3 + 1)
+        );
+    }
+}
+**/
+
 public class Collatz {
     public static long conjecture(long x) {
         int i = 1;
+        if (x == 0) {
+            return 0;
+        }
+
         do {
             if (x % 2 == 0) {
                 x = x / 2;
--- test/katatraining/collatzcojnjecturelength/CollatzTest.java
+++ test/katatraining/collatzcojnjecturelength/CollatzTest.java
@@ -2,19 +2,20 @@ package katatraining.collatzcojnjecturelength;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class CollatzTest {
 
     @Test
     public void testSomething() {
-        assertEquals("Test failed for: " + 100L, 26L, Collatz.conjecture(100L));
-        assertEquals("Test failed for: " + 10L, 7L, Collatz.conjecture(10L));
-        assertEquals("Test failed for: " + 500L, 111L, Collatz.conjecture(500L));
-        assertEquals("Test failed for: " + 1000000000L, 101L, Collatz.conjecture(1000000000L));
-        assertEquals("Test failed for: " + 1000000000000000L, 276L, Collatz.conjecture(1000000000000000L));
+        assertEquals(26L, Collatz.conjecture(100L), "Test failed for: " + 100L);
+        assertEquals(88L, Collatz.conjecture(103L), "Test failed for: " + 103L);
+        assertEquals(0L, Collatz.conjecture(0L), "Test failed for: " + 0L);
+        assertEquals(7L, Collatz.conjecture(10L), "Test failed for: " + 10L);
+        assertEquals(111L, Collatz.conjecture(500L), "Test failed for: " + 500L);
+        assertEquals(101L, Collatz.conjecture(1000000000L), "Test failed for: " + 1000000000L);
+        assertEquals(276L, Collatz.conjecture(1000000000000000L), "Test failed for: " + 1000000000000000L);
     }
 
-    private void assertEquals(String s, long l, long conjecture) {
-    }
 }
