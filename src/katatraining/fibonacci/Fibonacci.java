288da48a726e8b08bfc42786cb0ab90cf036d1fa
dima udodik <dima udodik>
2022-08-16 09:13:29 +0300

    Kata 7, "Fibonacci=)=)=)=)=)". Add 1 line into test. Exception is solved, but I don't understand this solution fully...

--- src/katatraining/fibonacci/Fibonacci.java
+++ src/katatraining/fibonacci/Fibonacci.java
@@ -2,7 +2,7 @@ package katatraining.fibonacci;
 
 public class Fibonacci {
     public static long fib(int n) {
-        int[] x = new int[n + 1];
+        int[] x = new int[n + 2];
         x[0] = 0;
         x[1] = 1;
         for (int i = 2; i <= n; i++) {
