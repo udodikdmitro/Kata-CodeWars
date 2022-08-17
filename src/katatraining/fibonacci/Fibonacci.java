01892cad5a9b205cea30a9f1a4c92dc62b1b2645
dima udodik <dima udodik>
2022-08-17 09:04:02 +0300

    Kata 7, "Fibonacci=)=)=)=)=)". Solution of this kata without creating array.

--- src/katatraining/fibonacci/Fibonacci.java
+++ src/katatraining/fibonacci/Fibonacci.java
@@ -2,12 +2,33 @@ package katatraining.fibonacci;
 
 public class Fibonacci {
     public static long fib(int n) {
-        int[] x = new int[n + 2];
-        x[0] = 0;
-        x[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            x[i] = x[i - 1] + x[i - 2];
+        int a = 0;
+        int b = 1;
+        if (n == 0) {
+            return a;
+        } else if (n == 1) {
+            return  b;
+        } else {
+            int c = 0;
+            for (int i = 2; i <= n; i++) {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            return c;
         }
-        return x[n];
+
     }
 }
+
+
+//        public static long fib(int n) {
+//        int[] x = new int[n + 2];
+//        x[0] = 0;
+//        x[1] = 1;
+//        for (int i = 2; i <= n; i++) {
+//            x[i] = x[i - 1] + x[i - 2];
+//        }
+//        return x[n];
+//    }
+//}
