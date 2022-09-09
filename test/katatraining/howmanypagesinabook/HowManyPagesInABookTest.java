847d5921b8bbd710981a2cf35f9471a72cb9f84a
dima udodik <dima udodik>
2022-09-09 20:31:12 +0300

    Kata 6. Pages of a book.

---
+++ test/katatraining/howmanypagesinabook/HowManyPagesInABookTest.java
@@ -0,0 +1,17 @@
+package katatraining.howmanypagesinabook;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HowManyPagesInABookTest {
+
+    @Test
+    public void testSample() {
+        assertEquals(  5, HowManyPagesInABook.amountOfPages(5));
+        assertEquals( 17, HowManyPagesInABook.amountOfPages(25));
+        assertEquals(401, HowManyPagesInABook.amountOfPages(1095));
+        assertEquals( 97, HowManyPagesInABook.amountOfPages(185));
+        assertEquals(256, HowManyPagesInABook.amountOfPages(660));
+    }
+}
