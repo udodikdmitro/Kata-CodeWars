2cc3f7db9dd5ddecf0bf829b4a1eddc42b462255
udodikdmitro <105604449+udodikdmitro@users.noreply.github.com>
2022-10-03 22:53:21 +0300

    Kata 6. Pages of a book. (#11)
    Co-authored-by: dima udodik <dima udodik>

--- src/katatraining/howmanypagesinabook/HowManyPagesInABook.java
+++ src/katatraining/howmanypagesinabook/HowManyPagesInABook.java
@@ -1,12 +1,20 @@
 package katatraining.howmanypagesinabook;
 
 public class HowManyPagesInABook {
-
     public static int amountOfPages(int summary) {
+        int k = 0;
         int n = 0;
-        int length = (int) (Math. log10(summary) + 1);
-        for (int i = 1; i <= length; i++) {
 
+        while (summary > 0) {
+            int a = 9 * (int) Math.pow(10, k);
+            if ((summary - a * (k + 1)) > 0) {
+                n = n + a;
+            } else {
+                n = n + summary / (k + 1);
+                break;
+            }
+            summary = summary - (k + 1) * a;
+            k++;
         }
         return n;
     }
